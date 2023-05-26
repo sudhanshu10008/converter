e9fc1469333a08b89a927a90e9051b7dae168cda
sudhanshu <sudhanshunagar555@gmail.com>
2023-05-26 18:48:28 +0530

    some upsdate

---
+++ README.md
@@ -0,0 +1,69 @@
+### Welcome to my repo
+
+<br>
+
+# **Features**
+This is powerful unit converter  
+list of converter
+
+<br>
+
+# **how to use**
+
+## for linux
+first you need to install python in your computer  
+go to --> https://www.python.org/  
+download and install it  
+<br>
+- update 
+  ```
+  sudo apt update
+  ```
+- Open terminal and install git
+
+    ```
+    sudo apt install git
+    ```
+- Clone repo in your pc
+  
+    ```
+    git clone 
+    ```
+- open directory
+    ```
+    cd converter
+    ```
+- run converter.py
+  ```
+  python converter.py
+  ```
+
+## for termux
+first you need to install python in your computer  
+go to --> https://www.python.org/  
+download and install it  
+<br>
+- update 
+  ```
+  apt update
+  ```
+- Open terminal and install git
+
+    ```
+    apt install git
+    ```
+- Clone repo in your pc
+  
+    ```
+    git clone 
+    ```
+- open directory
+    ```
+    cd converter
+    ```
+- run converter.py
+  ```
+  python converter.py
+  ```
+<br>
+
---
+++ gui/gui.py
@@ -0,0 +1,60 @@
+import tkinter as tk
+import ttkbootstrap as tb
+from ttkbootstrap.constants import *
+from converter_dict import dictionary as dic
+from ttkbootstrap.scrolled import ScrolledFrame
+
+
+# converter= [
+#         'Area Converter',
+#         "Angle Converter",
+#         'Length Converter',
+#         'Time Converter',
+#         "Volume Converter",
+#         'Weight Converter',
+#         "Speed Converter",
+#         "Storage Converter",
+#         "Energy Converter",
+#         'Temperature Converter',
+#     ]
+converter_name = dic["converter"]
+converter_name.sort()
+
+class Converter_gui:
+    def __init__(self) -> None:
+        self.root = tb.Window(title="Converter",size=(720,480))
+        
+        #converter button frame
+        self.left_side_frame = ScrolledFrame(master=self.root,padding=2,name="all",width=180, autohide=True)
+        self.left_side_frame.pack(fill=BOTH, side=LEFT, padx=1, pady=2)
+        
+        #main converter frame
+        self.right_side_frame = tb.Frame(master=self.root,padding=2,width=180)
+        self.right_side_frame.pack(fill=BOTH, side=LEFT)
+        
+        #main converter ui
+        self.con_to_frame = ScrolledFrame(master=self.right_side_frame,padding=2, width=180, autohide=True)
+        self.con_to_frame.pack(fill=BOTH, side=LEFT, padx=1, pady=2)
+        
+        for i in dic[]:
+            pass
+        
+        self.con_in_frame = ScrolledFrame(master=self.right_side_frame,padding=2, width=180, autohide=True)
+        self.con_in_frame.pack(fill=BOTH, side=RIGHT, padx=1, pady=2)
+        
+        
+        #all converter buttons
+        for i in converter_name:
+            name = i.split()[0]
+            self.name = tb.Button(master=self.left_side_frame, text=name, width=15)
+            self.name.pack(side=TOP, pady=5)
+            
+        
+        
+        
+        
+        
+        self.root.mainloop()
+        
+        
+c = Converter_gui()
